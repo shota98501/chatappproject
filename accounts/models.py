026fe90a3c6138c1f5769b5514e313244f301269
shota98501 <114859169+shota98501@users.noreply.github.com>
2024-06-08 16:14:24 +0900

    push

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, UserManager
 
-# Create your models here.
+class CustmomUser(AbstractBaseUser, UserManager)
+email = models.EmailField(verbose_name="メールアドレス",unique=True, blank=False, null=False)
+thumbnail = models.ImageField(upload_to="images/thumnbnail/", verbose_name="サムネイル", blank=True, null=True)
+USERNAME_FIELD = 'email'
+REQUUIRED_FIELDS =['username']
+
+def _str_(self):
+    return self.email
